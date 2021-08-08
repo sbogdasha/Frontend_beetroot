f609a325f487b6cb885c0c051c733e90a2ddcc34
Bogdan Stukalo <bogdan_stukalo@ukr.net>
2021-08-08 10:57:43 +0300

    check the bug

--- final-project/my-app/src/App.js
+++ final-project/my-app/src/App.js
@@ -1,12 +1,15 @@
 import './App.scss';
 import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
+import About from './components/About/About';
+
 
 function App() {
   return (
     <div className="App">
         <Header/>
         <Welcome/>
+        <About/>
     </div>
   );
 }
---
+++ final-project/my-app/src/components/About /About.js
@@ -0,0 +1,11 @@
+import './About.scss';
+
+export default function About() {
+    return (
+        <section className="About" id="About">
+            <div className="container">
+
+            </div>
+        </section>
+    );
+}
