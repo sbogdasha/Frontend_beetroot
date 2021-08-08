967bcfc1e194c72f8fe608acb6273ae12e04e1dc
Bogdan Stukalo <bogdan_stukalo@ukr.net>
2021-08-08 11:27:42 +0300

    fixed bugs

---
+++ final-project/my-app/src/components/About/About.js
@@ -0,0 +1,12 @@
+import './About.scss';
+
+
+export default function About(){
+    return (
+        <section id="about" className="about">
+            <div>
+
+            </div>
+        </section>
+    );
+}
